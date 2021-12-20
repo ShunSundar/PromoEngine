f487b1d9c2a0e4481af27d8de1d8d75a32d20565
Sundar <56379018+ShunSundar@users.noreply.github.com>
2021-12-20 13:52:54 +0000

    Adding Algorithm for rateBased Discount Calculations

--- src/main/java/org/example/promotionengine/algorithm/discount/RateBasedDiscountCalculator.java
+++ src/main/java/org/example/promotionengine/algorithm/discount/RateBasedDiscountCalculator.java
@@ -1,13 +1,23 @@
 package org.example.promotionengine.algorithm.discount;
 
 import org.example.promotionengine.models.CartItem;
+import org.example.promotionengine.models.Product;
 import org.example.promotionengine.models.PromotionItem;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 
 public class RateBasedDiscountCalculator implements DiscountCalculator{
+    private static final MathContext MATH_CONTEXT = new MathContext(4);
     @Override
     public BigDecimal getPromotionDiscount(CartItem cartItem, PromotionItem promotionItem) {
-        return null;
+        if(cartItem != null && promotionItem != null && promotionItem.getPromotionAmount().compareTo(BigDecimal.ZERO) > 0) {
+            Product product = cartItem.getProduct();
+            BigDecimal productPrice = product.getProductPrice();
+            BigDecimal rate = promotionItem.getPromotionAmount();
+            return productPrice.multiply(rate.divide(BigDecimal.valueOf(100),MATH_CONTEXT));
+        } else {
+            return BigDecimal.ZERO;
+        }
     }
 }
